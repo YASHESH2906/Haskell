1c03d40afd1703a7bc66d348deb638a310c050af
Yashesh Thakkar <thakkaryashesh29@gmail.com>
2026-03-12 11:50:24 +0530

    Delete Count.hs

--- Count.hs
+++
@@ -1,7 +0,0 @@
--- Count the number of elements in a list without using length
-count :: [a] -> Int
-count [] = 0
-count (_:xs) = 1 + count xs
-main :: IO ()
-main = do
-    print(count [1, 2, 3, 4, 5])
